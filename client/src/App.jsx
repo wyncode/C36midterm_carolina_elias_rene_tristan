ba47ddda298e225df08e33b6ad3dd0e2ca1e2948
Carolina Klein <carolina.e.klein@gmail.com>
2020-02-15 09:24:41 -0500

    added animal.json

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,10 @@
+import axios from 'axios'
 import React from 'react';
 import './App.css';
 
 class App extends React.Component {
   state = { serverMessage: '' };
 
-  componentDidMount() {
-    fetch('/api/demo')
-      .then(response => response.json())
-      .then(data => this.setState({ serverMessage: data.message }));
-  }
 
   render() {
     return (
