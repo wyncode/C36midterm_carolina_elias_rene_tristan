7b98e8e8d962b437ca6cc4cf10ff76c0b72be617
renebreslauer <renebreslauer@me.com>
2020-02-17 17:29:47 -0500

    homepage-animation

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import '../App.css';
+import Animation from '../Animation/animation';
 import Carousel from './Carousel';
 import HomeBanner from './HomeBanner';
-import { Container } from 'react-bootstrap';
+import { Container, Nav } from 'react-bootstrap';
 import '../App.css';
 
 const Home = () => {
   // render() {
   return (
     <Container className="banner-container" fluid={true}>
+      <Animation />
       <HomeBanner />
       <div id="carousel-div">
         <Carousel />
