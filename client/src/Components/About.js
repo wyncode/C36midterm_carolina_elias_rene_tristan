3306aaa5c7458306443fc5177158216956864ae1
renebreslauer <renebreslauer@me.com>
2020-02-18 18:48:50 -0500

    style changes to cards, about me

--- client/src/Components/About.js
+++ client/src/Components/About.js
@@ -1,47 +1,63 @@
 import React from 'react';
-import '../../src/App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
+import { Container, Col } from 'react-bootstrap';
+import ReactDOM from 'react-dom';
 
 const About = () => {
-
-    let teamMembers = [
-        {
-          name: "Carolina",
-          city: "Palma de Mallorca",
-          about: "I adore all animals and truly enjoyed collaborating on the creation of the FurEver App",
-          future: "I look forward to becoming a Software Engineer based in Miami, where I can continue to work with like-minded people ",
-        },
-        {
-          name: "Elias",
-          city: "Mexico City",
-          about: "something",
-          future: "else",
-        },
-        {
-          name: "Rene",
-          city: "Miami",
-          about: "something",
-          future: "else",
-        },
-        {
-          name: "Tristan",
-          city: "Miami",
-          about: "I like computers, games, anime , music , manga, and movies",
-          future: "In the future I want to be part of a team that creates the next big thing.",
-        }
-      ];
+  let teamMembers = [
+    {
+      // className: 'carolina-about',
+      name: 'Carolina',
+      city: 'Palma de Mallorca',
+      about:
+        'I adore all animals and truly enjoyed collaborating on the creation of the FurEver App',
+      future:
+        'I look forward to becoming a Software Engineer based in Miami, where I can continue to work with like-minded people'
+    },
+    {
+      name: 'Elias',
+      city: 'Mexico City',
+      about: 'something',
+      future: 'else'
+    },
+    {
+      name: 'Rene',
+      city: 'Miami',
+      about:
+        'Having grown up around cats, dogs, and horses, animals and animal welfare has always been an important part of my life. I now have three pet rabbits who destroy everything they touch, but I love them anyways',
+      future: 'else'
+    },
+    {
+      name: 'Tristan',
+      city: 'Miami',
+      about: 'I like computers, games, anime , music , manga, and movies',
+      future:
+        'In the future I want to be part of a team that creates the next big thing.'
+    }
+  ];
+  return (
+    <div className="team-member-container">
+      {teamMembers.map(teamMember => {
         return (
-          <div>
-            {
-              teamMembers.map(teamMember => {
-                return (<h1>{`Hi, I'm ${teamMember.name} and I'm from ${teamMember.city} .${teamMember.about} .${teamMember.future}!`}</h1>);  
-              })
-            }
-          </div>
-        )
-      
-}
+          <Container>
+            <Col>
+              <div className="flip-card">
+                <div className="flip-card-inner">
+                  <div className="flip-card-front">
+                    <p>image goes here</p>
+                  </div>
+                  <div className="flip-card-back">
+                    <h1>{`${teamMember.name}`}</h1>
+                    <p>{`I'm from ${teamMember.city} .${teamMember.about} .${teamMember.future}!`}</p>
+                  </div>
+                </div>
+              </div>
+            </Col>
+          </Container>
+        );
+      })}
+    </div>
+  );
+};
 
-export default About
+export default About;
--- client/src/Components/Cards.js
+++ client/src/Components/Cards.js
@@ -5,84 +5,97 @@ import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import '../App.css'
-
+import '../App.css';
 
 const filterOptions = [
-    { label: 'All', reset: true },
-    { value: 'Young', type: 'age' },
-    { value: 'Adult', type: 'age' },
-    { value: 'Senior', type: 'age' },
-    { value: 'Male', type: 'gender' },
-    { value: 'Female', type: 'gender' },
-    { value: 'Small', type: 'size' },
-    { value: 'Medium', type: 'size' },
-    { value: 'Large', type: 'size' },
-    { value: 'Xlarges', label: 'Extra Large', type: 'size' },
-]
+  { label: 'All', reset: true },
+  { value: 'Young', type: 'age' },
+  { value: 'Adult', type: 'age' },
+  { value: 'Senior', type: 'age' },
+  { value: 'Male', type: 'gender' },
+  { value: 'Female', type: 'gender' },
+  { value: 'Small', type: 'size' },
+  { value: 'Medium', type: 'size' },
+  { value: 'Large', type: 'size' },
+  { value: 'Xlarges', label: 'Extra Large', type: 'size' }
+];
 
-const Cards = ()=>{
+const Cards = () => {
+  const [pets, setPets] = useState([]);
+  const [filters, setFilters] = useState({});
 
-    const [pets, setPets] = useState([]);
-    const [filters, setFilters] = useState({})
+  useEffect(() => {
+    axios.get('/api/animals').then(res => setPets(res.data));
+  }, []);
 
-    useEffect(()=>{
-         axios.get('/api/animals')
-        .then(res => setPets(res.data))
-    }, [])
+  console.log('what', pets);
 
-    console.log('what', pets);
+  const updateFilters = ({ filterType, filterValue, reset }) =>
+    setFilters(
+      reset
+        ? {} // if reset is true empty the object
+        : { ...filters, [filterType]: filterValue } // if it's not true maintain the rest of the properties and append/update the new ones
+    );
 
-    const updateFilters = ({ filterType, filterValue, reset }) => setFilters(
-        reset 
-            ? {} // if reset is true empty the object
-            : { ...filters, [filterType]: filterValue } // if it's not true maintain the rest of the properties and append/update the new ones
-        );
-    
-    
-    const checkFilters = (filterKeys, pet) => filterKeys.every(key => { // we loop our filterd keys, and we compared the value of those filters with the values of each pet
-        const filterValue = filters[key];
-        return pet[key] === filterValue;
-    })
-   
-    return( // the return is what gets rendured 
-        <Container>
-            <div>
-            {filterOptions.map(({ value, label, type, reset }) => (
-                <button onClick={() => updateFilters({ filterType: type, filterValue: value, reset })}>{label || value}</button>
-            ))} {/* This filter options.map just maps through our options/filters /*/}
-            <Row>
+  const checkFilters = (filterKeys, pet) =>
+    filterKeys.every(key => {
+      // we loop our filterd keys, and we compared the value of those filters with the values of each pet
+      const filterValue = filters[key];
+      return pet[key] === filterValue;
+    });
 
-            {pets && pets.reduce((acc, pet) => { // the accumulator is a value that gets carried over after each accumulator
-                const filterKeys = Object.keys(filters) //array of strings for each key in the object
-                const hasFilters = !!filterKeys.length; //the !! are optional to further visualize that it's a falsy statement. 
+  return (
+    // the return is what gets rendured
+    <Container className="cards-container">
+      <div>
+        {filterOptions.map(({ value, label, type, reset }) => (
+          <button
+            onClick={() =>
+              updateFilters({ filterType: type, filterValue: value, reset })
+            }
+          >
+            {label || value}
+          </button>
+        ))}{' '}
+        {/* This filter options.map just maps through our options/filters /*/}
+        <Row>
+          {pets &&
+            pets.reduce((acc, pet) => {
+              // the accumulator is a value that gets carried over after each accumulator
+              const filterKeys = Object.keys(filters); //array of strings for each key in the object
+              const hasFilters = !!filterKeys.length; //the !! are optional to further visualize that it's a falsy statement.
 
-                if (!pet.photo) return acc; //if it doesn't have photos, skip this pet and return acc value
-                if (hasFilters) {
-                    const isValid = checkFilters(filterKeys, pet)
-                    if (!isValid) return acc; //if the filters aren't "valid", we skip the pet again by returning the accumulator, which is no pet, because we have set it to an empty array
-                }
-        
-                acc.push(( //so if it has fotos, and filters, push the pet "post" into the accumulator
-                    <Card key={pet.id} bg="dark" text="white" style={{ width: 250, height: 430, margin: 1, padding: 1 }}>
-                        <Card.Header  as="h3">{pet.name}</Card.Header>
-                        <Card.Img style = {{width: 220, height: 200, margin: 7, padding: 1}} src={pet.photo.full}/>
-                        <Card.ImgOverlay></Card.ImgOverlay>
+              if (!pet.photo) return acc; //if it doesn't have photos, skip this pet and return acc value
+              if (hasFilters) {
+                const isValid = checkFilters(filterKeys, pet);
+                if (!isValid) return acc; //if the filters aren't "valid", we skip the pet again by returning the accumulator, which is no pet, because we have set it to an empty array
+              }
 
-                        <Card.Body style = {{width: 200}}>
-                            <Card.Title></Card.Title>
-                            <Card.Subtitle as="h5">{pet.breed}</Card.Subtitle>
-                            <br></br>
-                            <Card.Footer><small>Age: {pet.age} <br/> Size: {pet.size} <br/>  Sex: {pet.gender} </small></Card.Footer>
-                        </Card.Body>
-                    </Card> 
-                ))
-                return acc;
-            }, [])} {/* this empty braket (empty array) represents the innitial accumulator value*/}
-            </Row>
-          </div>  
-        </Container>    
-    ) 
-}
+              acc.push(
+                //so if it has fotos, and filters, push the pet "post" into the accumulator
+                <Card key={pet.id}>
+                  <Card.Header>{pet.name}</Card.Header>
+                  <Card.Img src={pet.photo.full} />
+                  <Card.ImgOverlay></Card.ImgOverlay>
+
+                  <Card.Body>
+                    <Card.Title></Card.Title>
+                    <Card.Subtitle>{pet.breed}</Card.Subtitle>
+                    <br></br>
+                    <Card.Footer>
+                      Age: {pet.age} <br /> Size: {pet.size} <br /> Sex:{' '}
+                      {pet.gender}{' '}
+                    </Card.Footer>
+                  </Card.Body>
+                </Card>
+              );
+              return acc;
+            }, [])}{' '}
+          {/* this empty braket (empty array) represents the innitial accumulator value*/}
+        </Row>
+      </div>
+    </Container>
+  );
+};
 
 export default Cards;
