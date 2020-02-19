002b77ecb3fcdfc6dcd6e3a26d920a6822849ffc
renebreslauer <renebreslauer@me.com>
2020-02-18 19:14:40 -0500

    nav bar fix

--- client/src/Components/Nav.jsx
+++ client/src/Components/Nav.jsx
@@ -3,68 +3,86 @@ import logo from '../Images/logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import '../App.css';
-import { Link } from 'react-router-dom';
-
+import { Link } from 'react-rouyter-dom';
 
 const MyNav = () => {
   return (
     <Navbar expand="lg">
-      <Nav.Link href="home">
-        <Link to ="/"><img src={logo} className="logo" alt="logo"></img> </Link>
-      </Nav.Link>
+      <Nav.Item href="home">
+        <Link to="/">
+          <img src={logo} className="logo" alt="logo"></img>{' '}
+        </Link>
+      </Nav.Item>
 
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto"></Nav>
 
-      <Nav.Link>
-         <Link className="nav-link" to="/dogs">Dogs</Link> 
-      </Nav.Link>   
+        <Nav.Item>
+          <Link className="nav-link" to="/dogs">
+            Dogs
+          </Link>
+        </Nav.Item>
 
-      <Nav.Link>
-         <Link className="nav-link" to="/cats">Cats</Link>
-      </Nav.Link>
+        <Nav.Item>
+          <Link className="nav-link" to="/cats">
+            Cats
+          </Link>
+        </Nav.Item>
 
-       <NavDropdown title="Small Animals"  id="basic-nav-dropdown">
-         <Nav.Link>
-           <NavDropdown.Item>
-             <Link className="nav-link" to="/rabbits">Rabbits</Link>
-           </NavDropdown.Item>
-         </Nav.Link>
-         <Nav.Link>
-             <NavDropdown.Item>
-               <Link className="nav-link" to="/rodents">Rodents</Link>
+        <NavDropdown title="Small Animals" id="basic-nav-dropdown">
+          <Nav.Item>
+            <NavDropdown.Item>
+              <Link className="nav-link" to="/rabbits">
+                Rabbits
+              </Link>
             </NavDropdown.Item>
-           </Nav.Link>
-           <Nav.Link>
-           <NavDropdown.Item>
-             <Link className="nav-link" to="/ferrets">Ferrets</Link>
+          </Nav.Item>
+          <Nav.Item>
+            <NavDropdown.Item>
+              <Link className="nav-link" to="/rodents">
+                Rodents
+              </Link>
             </NavDropdown.Item>
-           </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <NavDropdown.Item>
+              <Link className="nav-link" to="/ferrets">
+                Ferrets
+              </Link>
+            </NavDropdown.Item>
+          </Nav.Item>
         </NavDropdown>
 
         <NavDropdown title="Other Critters" id="basic-nav-dropdown">
-        <Nav.Link>
-           <NavDropdown.Item>
-             <Link className="nav-link" to="/birds">Birds</Link>
+          <Nav.Item>
+            <NavDropdown.Item>
+              <Link className="nav-link" to="/birds">
+                Birds
+              </Link>
             </NavDropdown.Item>
-           </Nav.Link>
-           <Nav.Link>
-           <NavDropdown.Item>
-             <Link className="nav-link" to="/reptiles">Reptiles</Link>
+          </Nav.Item>
+          <Nav.Item>
+            <NavDropdown.Item>
+              <Link className="nav-link" to="/reptiles">
+                Reptiles
+              </Link>
             </NavDropdown.Item>
-           </Nav.Link>
-           <Nav.Link>
-           <NavDropdown.Item>
-             <Link className="nav-link" to="/fish">fish</Link>
+          </Nav.Item>
+          <Nav.Item>
+            <NavDropdown.Item>
+              <Link className="nav-link" to="/fish">
+                fish
+              </Link>
             </NavDropdown.Item>
-           </Nav.Link>
+          </Nav.Item>
         </NavDropdown>
-  
 
         <Nav.Item as="li">
           <Nav.Link eventKey="link-1">
-            <Link className="nav-link" to="/about">About Us</Link>
+            <Link className="nav-link" to="/about">
+              About Us
+            </Link>
           </Nav.Link>
         </Nav.Item>
       </Navbar.Collapse>
