d1a1e2074a48308119e8c1ef6fe387656e07b30e
renebreslauer <renebreslauer@me.com>
2020-02-22 08:25:33 -0500

    minor styling updates and card details

--- client/src/Components/PetDetail.js
+++ client/src/Components/PetDetail.js
@@ -27,15 +27,18 @@ const PetDetail = () => {
   }, []);
   return (
     <div className="petDetailsContainer">
-      <div>
-        <Container>
-          <Row>
-            <Col md="auto">
+      <Container>
+        <Row>
+          <Col md="auto">
+            <div className="petCarousel">
               <Carousel>
                 {petData.image &&
                   petData.image.map(image => {
                     return (
-                      <Carousel.Item style={{ width: '18rem' }}>
+                      <Carousel.Item
+                        className="carouselPic"
+                        style={{ width: '18rem' }}
+                      >
                         <Card.Img
                           variant="top"
                           src={image.full}
@@ -45,33 +48,32 @@ const PetDetail = () => {
                     );
                   })}
               </Carousel>
-            </Col>
-            <Col md="auto">
-              <Card>
-                <Card.Body>
-                  Name: {petData.name && petData.name} <br />
-                  Breed: {petData.breeds && petData.breeds.primary} <br />
-                  Age: {petData.age && petData.age} <br />
-                  Size: {petData.size && petData.size} <br /> <br /> <br />
-                  Contact <br />
-                  Email: {petData.contact && petData.contact.email} <br />
-                  Phone: {petData.contact && petData.contact.phone} <br />
-                  Address: {petData.contact &&
-                    petData.contact.address.address1}{' '}
-                  <br />
-                  {petData.contact && petData.contact.address.address2} <br />
-                  City: {petData.contact && petData.contact.address.city} <br />
-                  State: {petData.contact && petData.contact.address.state}{' '}
-                  <br />
-                  Zipcode: {petData.contact &&
-                    petData.contact.address.postcode}{' '}
-                  <br />
-                </Card.Body>
-              </Card>
-            </Col>
-          </Row>
-        </Container>
-      </div>
+            </div>
+          </Col>
+          <Col md="auto">
+            <div className="InfoCard">
+              Name: {petData.name && petData.name} <br />
+              Breed: {petData.breeds && petData.breeds.primary} <br />
+              Age: {petData.age && petData.age} <br />
+              Size: {petData.size && petData.size} <br /> <br /> <br />
+              Contact <br />
+              Email:{' '}
+              <a href="mailto:">{petData.contact && petData.contact.email}</a>
+              <br />
+              Phone: {petData.contact && petData.contact.phone} <br />
+              Address: {petData.contact &&
+                petData.contact.address.address1}{' '}
+              <br />
+              {petData.contact && petData.contact.address.address2} <br />
+              City: {petData.contact && petData.contact.address.city} <br />
+              State: {petData.contact && petData.contact.address.state} <br />
+              Zipcode: {petData.contact &&
+                petData.contact.address.postcode}{' '}
+              <br />
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
